d440c63293b72088dd7967da259a94fc5a89ec68
Sreelatha Vallabhaneni <sreelatha.vallabhaneni@gmail.com>
2020-05-04 02:35:12 +0200

    updated home page

--- src/components/home.component.js
+++ src/components/home.component.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import myImage from '../images/sreelatha.jpg';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedinIn, faGithub} from "@fortawesome/free-brands-svg-icons";
+import "./home.styles.css";
+
 export default function home(){
     return (
-      <div>
-        <span>
-          <p>I'M Sreelatha Vallabhaneni</p>
-          <p>A Full-Stack Web Developer.</p>
-        </span>
-        <img src={myImage} alt="sreelatha vallabhaneni"/>
+      <div className="home-container">
+        <div>
+          <h1 className="content-name">I'M Sreelatha Vallabhaneni</h1>
+          <h4 className="content-role">
+            <span className="alphabet-A">A</span> Full-Stack Web Developer.
+          </h4>
+          <button className="linkedin">
+            <FontAwesomeIcon className="icon-link" icon={faLinkedinIn} />
+          </button>
+          <button className="linkedin">
+            <FontAwesomeIcon className="icon-link" icon={faGithub} />
+          </button>
+        </div>
+        <img className="my-image" src={myImage} alt="sreelatha vallabhaneni" />
       </div>
     );
 };
