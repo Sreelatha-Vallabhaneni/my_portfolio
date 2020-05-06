4aa10bb96556fae43b5b43cafdc11f9a7d35c3e6
Sreelatha Vallabhaneni <sreelatha.vallabhaneni@gmail.com>
2020-05-07 00:57:01 +0200

    added route to navigation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import SideNavBar from './components/navBar.component';
 import Home from './components/home.component';
+import About from './components/about.component';
 
-function App() {
+function App() {  
   return (
-    <div className="App">
-      <SideNavBar/>
-      <Home/>
-    </div>
+    <Router>
+      <div className="App">
+        <SideNavBar />
+        <Switch>
+          <Route exact path="/" component={Home}></Route>
+          <Route path="/about" component={About}></Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/components/navBar.component.js
+++ src/components/navBar.component.js
@@ -1,20 +1,23 @@
 import React from 'react';
+import { useHistory } from "react-router-dom";
+//import { withRouter } from "react-router";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHome, faUser, faEnvelope, faFile, faWindowRestore } from "@fortawesome/free-solid-svg-icons";
 import './navBar.styles.css';
 
-export default function sideNav(){
+function SideNav(){
+  let history = useHistory();
     return (
       <div className="navbar-container">
         <span className="nav-txt">
           <p>Sreelatha</p>
-          <p>Vallabhaneni</p>          
+          <p>Vallabhaneni</p>
         </span>
-        <button className="nav-btn">
+        <button className="nav-btn" onClick={() => history.push("/")}>
           <FontAwesomeIcon className="icon-title" icon={faHome} />
           Home
         </button>
-        <button className="nav-btn">
+        <button className="nav-btn" onClick={() => history.push("/about")}>
           <FontAwesomeIcon className="icon-title" icon={faUser} />
           About
         </button>
@@ -33,3 +36,6 @@ export default function sideNav(){
       </div>
     );
 }
+
+// export default withRouter(SideNav);
+export default SideNav;
