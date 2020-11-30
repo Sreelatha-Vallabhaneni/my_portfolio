608725e00bcdbf077222932dfe343c7602193cd1
Sreelatha Vallabhaneni <sreelatha.vallabhaneni@gmail.com>
2020-11-30 23:57:42 +0100

    updated

--- src/components/home page/home.component.js
+++ src/components/home page/home.component.js
@@ -5,20 +5,18 @@ import "./home.styles.css";
 export default function home(){
   const listOfString=["Full-Stack Web Developer.", "Software Tester."]
     return (
-      <React.Fragment>
-        <div className="home-container">
-          <div className="res-text">
-            <div className="home-text">
-              <h1 className="content-name">I'M Sreelatha Vallabhaneni</h1>
-              <h4 className="content-role">
-                <span className="alphabet-A">A </span>
-                <div>
-                  <ReactJsTyping StringList={listOfString} speed={1000} />
-                </div>
-              </h4>
-            </div>
+      <div className="home-container">
+        <div className="res-text">
+          <div className="home-text">
+            <h1 className="content-name">I'M Sreelatha Vallabhaneni</h1>
+            <h4 className="content-role">
+              <span className="alphabet-A">A </span>
+              <div>
+                <ReactJsTyping StringList={listOfString} speed={1000} />
+              </div>
+            </h4>
           </div>
         </div>
-      </React.Fragment>
+      </div>
     );
 };
