c341d4813133ef5a3eaa24c0237b506d2c66bafc
Sreelatha Vallabhaneni <sreelatha.vallabhaneni@gmail.com>
2020-12-03 02:33:30 +0100

    deleted image and comments

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import "./App.css";
 
-//import SideNavBar from './components/side navbar/navBar.component';
 import SideNavBar from "./components/side navbar/burgerMenu.component";
 import Home from './components/home page/home.component';
 import About from './components/about page/about.component';
